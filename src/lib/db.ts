284b5a91e22ee70f684ac2686495a0b7fe125062
vishal-gawade-031 <vishalgavade031@gmail.com>
2026-03-26 00:18:21 +0530

    simple email and password credentials

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,8 +2,33 @@ import mongoose  from "mongoose";
 
 //create the connection and save it 
 
-const mongooseUrl=process.env.MONGODB_URL
+const mongodbUrl=process.env.MONGODB_URL
 
-if(!mongooseUrl){
+if(!mongodbUrl){
     throw new Error ("error in db");
 }
+
+let cached = global.mongoose
+if(!cached){
+    cached=global.mongoose={conn:null,promise:null}
+}
+
+//if cache get connection then it's ok but cache did't get connection then create it new 
+//connection of database
+const connectDb=async ()=>{
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+        cached.promise=mongoose.connect(mongodbUrl).then((conn)=>conn.connection)
+    }
+    try{
+        const conn=await cached.promise
+        return conn
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export default connectDb
