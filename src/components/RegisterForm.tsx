7f35b3d6a4ed5586a7fee5e1180344b4b4710690
vishal-gawade-031 <vishalgavade031@gmail.com>
2026-03-29 23:54:07 +0530

    handle register api

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,7 +1,9 @@
-import { ArrowLeft, EyeIcon, EyeOff, Key, Leaf, Lock, Mail, User, Vault } from 'lucide-react'
+import { ArrowLeft, EyeIcon, EyeOff, Key, Leaf, Lock, LogIn, Mail, User, Vault } from 'lucide-react'
 import { motion } from 'motion/react'
-import Email from 'next-auth/providers/email'
+import Image from 'next/image'
+import googleImage from '@/assets/google-logo-icon-gsuite-hd-701751694791470gzbayltphh.png'
 import React, { useState } from 'react'
+import axios from 'axios'
 
 type propType={
     previousStep:(s:number)=>void
@@ -11,6 +13,19 @@ const RegisterForm = ({previousStep}:propType) => {
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const [showPassword,setshowPassword]=useState(false);
+    
+    const handleRegister=async ()=>{
+        try{
+            const result=await axios.post("/api/auth/register",{
+                name,email,password
+            })
+            console.log(result.data)
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
   return (
         <div className='flex flex-col items-center justify-center min-h-screen px-6
         py-10 bg-white relative'>
@@ -89,9 +104,34 @@ const RegisterForm = ({previousStep}:propType) => {
                         onClick={()=>setshowPassword(false)}/>:<EyeIcon className='absolute right-3 top-3 w-5 h-5 text-gray-500 cursor-pointer' onClick={()=>setshowPassword(true)}/>
                       }
                       
-                      
                 </div>
+
+              {/* when we write the code in back tick then we can applay javascript register button*/}
+                      {
+                      (()=>{
+                        const formValidation = name !== "" && email !== "" && password !==""
+                        return <button className={`w-full font-semibold py-3 rounded-xl transition-all duration-200 shadow-md inline-flex items-center justify-center gap-2 ${
+                            formValidation
+                            ?"bg-green-600 hover:bg-green-700 text-white" : "bg-gray-300 text-gray-500 cursor-not-allowed"
+                        }`}>Register
+                        </button>
+
+                      })()}
+
+                      <div className='flex items-center gap-2 text-gray-400 text-sm mt-2'>
+                        <span className='flex-1 h-px bg-gray-400'></span> OR <span className='flex-1 h-px bg-gray-400'></span>
+                        </div>
+
+                        <button className='w-full flex items-center justify-center gap-3 border border-gray-300
+                        hover:bg-gray-50 py-3 rounded-b-xl text-gray-700 font-medium transition-all duration-200'>
+                            <Image src={googleImage} width={20} height={20} alt='google image'/>
+                            Continue with Google 
+                        </button>
+
             </motion.form>
+            <p className='text-gray-600 mt-6 text-sm flex items-center gap-1 cursor-pointer'>
+                Already have an account ? <LogIn className='w-4 h-4 cursor-pointer'/><span className='text-green-500'>Sign in</span></p>
+
         </div>
   )
 }
