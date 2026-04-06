7fa14725f6f0d9c994dc06dbcdee4500c9b7c143
vishal-gawade-031 <vishalgavade031@gmail.com>
2026-04-06 23:36:23 +0530

    Img added in nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import mongoose from 'mongoose'
-import { Search, ShoppingCart, ShoppingCartIcon } from 'lucide-react'
+import Image from "next/image"
+import { Search, ShoppingCart, ShoppingCartIcon, User } from 'lucide-react'
 interface IUser{
     _id?:mongoose.Types.ObjectId
     name:string
@@ -12,7 +13,7 @@ interface IUser{
     image?:string
 }
 function Nav({user}:{user:IUser}) {
-    console.log(user);
+   // console.log(user);
   return (
     <div className='w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700
     rounded-2xl shodow-lg shadow-black/30 flex justify-between items-center h-20 px-4
@@ -24,15 +25,22 @@ function Nav({user}:{user:IUser}) {
 
         <form className='hidden md:flex item-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md'>
         <Search className='text-gray-500 w-5 h-5 mr-2'/>
-        <input type="text" placeholder='Search groceries...' className='w-full outline-none text-gray-700 placeholder-gray-400' />
+        <input type="text" placeholder='Search for groceries...' className='w-full outline-none text-gray-700 placeholder-gray-400' />
         </form>
 
         <div className="flex items-center gap-3 md:gap-6 relative">
             <Link href={""} className='relative bg-white rounded-full w-11 h-11 flex items-center
             justify-center shadow-md hover:scale-150 transition'>
             <ShoppingCartIcon className='w-5 h-5'/>
-            <span></span>
+            <span className='absolute -top-1 -right-1 bg-red-500 text-white text
+             w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow'>0</span>
             </Link>
+
+            <div className="bg-white rounded-full w-11 h-11 flex items-center justify-center overflow-hidden shadow-md
+            hover:scal-105 transition-transform relative">
+                {/* image of google  */}
+                {user.image?<Image src={user.image} alt='image' fill className='object-cover rounded-full'/>:<User/>}
+            </div>
         </div>
 
     </div>
