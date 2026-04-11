9c1f3ad4527257beaa5b3d1996f58da995969f03
vishal-gawade-031 <vishalgavade031@gmail.com>
2026-04-12 03:41:12 +0530

    hero section added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images:{
     remotePatterns:[
-      {hostname:"lh3.googleusercontent.com"}
+      {hostname:"lh3.googleusercontent.com"},
+      {hostname:"plus.unsplash.com"},
+      {hostname:"images.unsplash.com"},
     ]
   }
 };
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,88 @@
-import React from 'react'
-
+'use client'
+import { Leaf, ShoppingBasket, Smartphone, Truck } from 'lucide-react'
+import { AnimatePresence } from 'motion/react'
+import { title } from 'process'
+import React, { useEffect, useState } from 'react'
+import { motion } from "motion/react"; 
+import Image from 'next/image'
 const HeroSection = () => {
-    
+    const slides=[{
+      id:1,
+      icon:<Leaf className='w-20 h-20 sm:w-28 sm:h-28 text-green-400 droup-shadow-lg'/>,
+      title:" Fresh Organic Groceries",
+      subtitle:"farm-fresh fruits, vegetavles,and daily essentials delivered to you",
+      btnText:"shop Now",
+      bg:"https://plus.unsplash.com/premium_photo-1661663164873-b30abc0bdd06?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
+{
+  id:2,
+  icon:<Truck className='w-20 h-20 sm:w-28 sm:h-28 text-yellow-400 droup-shadow-lg'></Truck>,
+  title:"Fast & Reliable Delivery",
+  subtitle:"We ensure your groceries reach your doorstep in no time",
+  btnText:"order now",
+  bg:"https://images.unsplash.com/photo-1617347454431-f49d7ff5c3b1?q=80&w=1115&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  
+},
+{
+  id:3,
+  icon:<Smartphone className='w-20 h-20 sm:w-28 sm:h-28 text-blue-400 droup-shadow-lg'/>,
+  title:"Best Prices Guaranteed",
+  subtitle:"Get the best deals on all your grocery needs",
+  btnText:"explore now",
+  bg:"https://plus.unsplash.com/premium_photo-1663089562105-77acad484412?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+}]
+
+const[current,setCurrent]=useState(0)
+useEffect(()=>{
+  const timer=setInterval(()=>{
+setCurrent(prev => (prev + 1) % slides.length)
+  },4000)
+  return ()=>clearInterval(timer)
+},[])
+
+  
   return (
-    <div>
-      
+    <div className='relative w-[95%] mx-auto mt-32 h-[80vh] rounded-3xl overflow-hidden shadow-2xl'> 
+      <AnimatePresence mode='wait'>
+<motion.div
+  key={current}
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 0.8 }}
+    exit={{opacity:0}}
+    className='absolute inset-0'
+>
+  <Image src={slides[current].bg}
+  fill
+  alt='slide'
+  priority
+  className='object-cover'/>
+  <div className="absolute inset-0 bg-black/50 backdrop-blur-[1px]"></div>
+  
+</motion.div>
+      </AnimatePresence>
+
+      <div className="absolute inset-0 flex items-center justify-center text-center text-white px-5">
+        <motion.div
+        initial={{
+          y:30,opacity:0
+        }}
+        animate={{y:0,opacity:1}}
+        className='flex flex-col items-center justify-center gap-6 max-w-3xl'
+        >
+          <div className='bg-white/10 backdrop-blur-md p-6 rounded-full shadow-lg '>{slides[current].icon}</div>
+          <h1 className='text-3xl sm:text-5xl md:text-6xl font-extrabold tracking-tight drop-shadow-lg'>{slides[current].title}</h1>
+          <p className='text-lg sm:text-xl text-gray-200 max-w-2xl'>{slides[current].subtitle}</p>
+          <motion.button
+          whileHover={{scale:1.09}}
+          whileTap={{scale:0.96}}
+          className='mt-4 bg-white text-green-700 hover:bg-green-100 px-8 py-3 rounded-full font-semibold
+          shadow-lg transition-all duration-300 flex items-center gap-2'>
+            <ShoppingBasket className='w-5 h-5'/>
+            {slides[current].btnText}
+          </motion.button>
+        </motion.div>
+      </div>
     </div>
   )
 }
