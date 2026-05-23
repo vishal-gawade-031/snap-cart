a40d3f3ea1c02ce26f533f4d2eddf0e29dcc1232
vishal-gawade-031 <vishalgavade031@gmail.com>
2026-05-23 23:56:23 +0530

    now we can see the redux state using redux devtool extension

--- src/redux/StoreProvider.tsx
+++ src/redux/StoreProvider.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 const StoreProvider = ({children}:{children:React.ReactNode}) => {
   return (
+    //children is provided to full children
     <Provider store={store}>
         {children}
     </Provider>
