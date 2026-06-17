c40138b9d1eb54f6bf27bac146c3ed7ae79404b5
vishal-gawade-031 <vishalgavade031@gmail.com>
2026-06-18 02:51:58 +0530

    added reducer to increase quantity

--- src/components/GroceryItemCard.tsx
+++ src/components/GroceryItemCard.tsx
@@ -1,78 +1,93 @@
-'use client'
-import mongoose from 'mongoose';
-import React from 'react'
-import {motion} from "motion/react"
-import Image from 'next/image';
-import { Minus, Plus, ShoppingCart } from 'lucide-react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDirModules } from 'next/dist/build/webpack/loaders/next-app-loader';
-import { AppDispatch, RootState } from '@/redux/store';
-import { addToCart } from '@/redux/cartSlice';
+"use client";
+import mongoose from "mongoose";
+import React from "react";
+import { motion } from "motion/react";
+import Image from "next/image";
+import { Minus, Plus, ShoppingCart } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDirModules } from "next/dist/build/webpack/loaders/next-app-loader";
+import { AppDispatch, RootState } from "@/redux/store";
+import { addToCart, increaseQuantity } from "@/redux/cartSlice";
 interface IGrocery {
-    _id?: mongoose.Types.ObjectId;
-    name: string;
-    category: string;
-    price: string;
-    unit: string;
-    image: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  category: string;
+  price: string;
+  unit: string;
+  image: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-function GroceryItemCard({item}:{item:IGrocery}) {
-    const dispatch=useDispatch<AppDispatch>()
+function GroceryItemCard({ item }: { item: IGrocery }) {
+  const dispatch = useDispatch<AppDispatch>();
   //console.log("item name:",item.name)
-  const {cartData}=useSelector((state:RootState)=>state.cart)
-  const cartItem = cartData.find(i => i._id === item._id);//it  returns a value not true or false
-  console.log("cartItem in groceryItemCard:",cartItem);
+  const { cartData } = useSelector((state: RootState) => state.cart);
+  const cartItem = cartData.find((i) => i._id === item._id); //it  returns a value not true or false
+  console.log("cartItem in groceryItemCard:", cartItem);
   return (
-      <motion.div
-       initial={{ opacity: 0, y: 50, scale:0.9}}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6 }}
-            viewport={{ once: false, amount: 0.3 }} 
-            className='bg-white rounded-2xl shadow-sm hover:shadow-xl transition-all duration-300 overflow-hidden
-             border border-gray-100 flex flex-col'
-            >
-                
-                <div className="w-full relative aspect-4/3 bg-gray-50 overflow-hidden group">
-                    <Image src={item.image} fill alt={item.name} sizes='(max-width:768px) 100vw,25vw' 
-                    className='object-contain p-4 transition-transform duration-500 group-hover:scale-150'/>
-                    <div className="absolute inset-0 bg-linear-to-t from-black/10 to-transparent opacity-0 group-hover:opacity-100
-                    transition-all duration-300">
-                    </div>
-                    </div>
-                    <div className='p-4 flex flex-col flex-1'>
-                          <p className='text-sm text-gray-500 font-medium mb-1'>{item.category}</p>
-                          <h3>{item.name}</h3>
-                            <div className='flex items-center justify-between mt-2'>
-                                <span className='text-xs font-medium text-gray-600 bg-gray-100 px-2 py-1 rounded-full'>
-                                {item.unit}
-                                </span>
-                                <span className='text-green-700 font-bold text-lg'>{item.price}</span>
-                                </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50, scale: 0.9 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      viewport={{ once: false, amount: 0.3 }}
+      className="bg-white rounded-2xl shadow-sm hover:shadow-xl transition-all duration-300 overflow-hidden
+             border border-gray-100 flex flex-col"
+    >
+      <div className="w-full relative aspect-4/3 bg-gray-50 overflow-hidden group">
+        <Image
+          src={item.image}
+          fill
+          alt={item.name}
+          sizes="(max-width:768px) 100vw,25vw"
+          className="object-contain p-4 transition-transform duration-500 group-hover:scale-150"
+        />
+        <div
+          className="absolute inset-0 bg-linear-to-t from-black/10 to-transparent opacity-0 group-hover:opacity-100
+                    transition-all duration-300"
+        ></div>
+      </div>
+      <div className="p-4 flex flex-col flex-1">
+        <p className="text-sm text-gray-500 font-medium mb-1">
+          {item.category}
+        </p>
+        <h3>{item.name}</h3>
+        <div className="flex items-center justify-between mt-2">
+          <span className="text-xs font-medium text-gray-600 bg-gray-100 px-2 py-1 rounded-full">
+            {item.unit}
+          </span>
+          <span className="text-green-700 font-bold text-lg">{item.price}</span>
+        </div>
 
-                                {!cartItem ?<motion.button className='mt-4 flex items-center justify-center gap-2 bg-green-600 hover:bg-green-700
-                                text-white rounded-full py-2 text-sm font-medium transition-all'
-                                onClick={()=>dispatch(addToCart({...item,quantity:1}))}
-                                >
-                                    <ShoppingCart/> Add to Cart
-                                </motion.button>:
-                                <motion.div
-                                initial={{opacity:0,y:10}}
-                                animate={{opacity:1,y:0}}
-                                className='mt-4 flex items-center justify-center bg-green-50 border border-green-200 rounded-full py-2
-                                px-4 gap-4'
-                                >
-                                    <button className='w-7 h-7 flex items-center justify-center rounded-full bg-green-100 hover:bg-green-200 transition-all'><Minus size={16}/></button>
-                                    <span>0</span>
-                                    <button className='w-7 h-7 flex items-center justify-center rounded-full bg-green-100 hover:bg-green-200 transition-all'><Plus size={16}/></button>
-                                </motion.div>
-                                }
-                    </div>
-                
-      </motion.div>
-  )
+        {!cartItem ? (
+          <motion.button
+            className="mt-4 flex items-center justify-center gap-2 bg-green-600 hover:bg-green-700
+                                text-white rounded-full py-2 text-sm font-medium transition-all"
+            onClick={() => dispatch(addToCart({ ...item, quantity: 1 }))}
+          >
+            <ShoppingCart /> Add to Cart
+          </motion.button>
+        ) : (
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{duration:0.3}}
+            className="mt-4 flex items-center justify-center bg-green-50 border border-green-200 rounded-full py-2
+                                px-4 gap-4"
+          >
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-green-100 hover:bg-green-200 transition-all">
+              <Minus size={16} />
+            </button>
+            <span className="text-sm font-semibold text-gray-800">{cartItem.quantity}</span>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-green-100 hover:bg-green-200 transition-all"
+             onClick={()=>dispatch(increaseQuantity(item._id))}>
+              <Plus size={16} />
+            </button>
+          </motion.div>
+        )}
+      </div>
+    </motion.div>
+  );
 }
 
-export default GroceryItemCard
+export default GroceryItemCard;
--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import mongoose from "mongoose";
 
 interface IGrocery {
-    id?: mongoose.Types.ObjectId;
+    id: mongoose.Types.ObjectId;
     name: string;
     category: string;
     price: string;
@@ -26,11 +26,26 @@ const cartSlice = createSlice({
   reducers: {
     addToCart:(state,action:PayloadAction<IGrocery>)=>{
         state.cartData.push(action.payload)
+    },
+    increaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
+      const item=state.cartData.find(i=>i._id == action.payload);
+      if(item){
+        console.log(item);
+        item.quantity=item.quantity + 1
+      }
+    },
+    decreaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
+          const item=state.cartData.find(i=>._id == action.payload);
+          if(item?.quantity && item.quantity > 1){
+            item.quantity= item.quantity - 1
+          }else{
+            
+          }
     }
   },
 });
 
 
 
-export const {addToCart}=cartSlice.actions
+export const {addToCart,increaseQuantity}=cartSlice.actions
 export default cartSlice.reducer;
